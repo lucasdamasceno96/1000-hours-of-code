4aea64924e48489591fc24f3f417808d159fc29e
Lucas Damasceno <lucasdamascenopinto96@gmail.com>
2025-03-29 20:30:09 -0300

    update

---
+++ subscription-tracker/app.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+
+import authRouter from './routes/auth.route.js';
+import useRouter from './routes/user.route.js';
+import subscriptionRouter from './routes/subscription.route.js';
+import connectDB from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api//v1/users', useRouter);
+app.use('/api//v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Subscription Tracker API');
+});
+
+app.listen(PORT, async () => {
+    console.log(`Subscription Tracker API is running on https://localhost:${PORT}`);
+
+    await connectDB();
+});
+
+export default app;
